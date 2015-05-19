44ac32243b39e47db17139dff15b7329605e0683
xiaopeng <lost1q84@gmail.com>
2015-05-19 19:59:13 +0800

    rewrite zhihuDate filter

--- app/filter/zhihuDate.filter.js
+++ app/filter/zhihuDate.filter.js
@@ -2,9 +2,13 @@
   'use strict';
 
   angular.module('app')
-    .filter('zhihuDate', function(){
-      return function(input) {
-        return input && input.substring(0, 4) + '-' + input.substring(4, 6) + '-' + input.substring(6, 8);
+    .filter('zhihuDate', function($filter){
+      return function(input, param) {
+        if(!input) {
+          return;
+        }
+        var date = input.substring(0, 4) + '-' + input.substring(4, 6) + '-' + input.substring(6, 8);
+        return $filter('date')(date, param);
       };
     });
 })();
